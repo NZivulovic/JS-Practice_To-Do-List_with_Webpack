dd01aa22dd9ef93e9e3f81fe5399df53596c147b
NZivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-12-27 18:19:11 +0100

    webpack finished setup

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
@@ -19,6 +18,14 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
             },
         ],
     },
